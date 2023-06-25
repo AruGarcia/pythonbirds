83b0c9c43ade144f49f68c96b322afcfcb8d159f
Aruana <aruanagarcia@gmail.com>
2023-06-25 17:41:49 -0300

    Criados atributos de nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self,  nome=None, idade=43):
+        self.idade = idade
+        self.nome = nome
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Eleonora')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Aruanã'
+    print(p.nome)
+    print(p.idade)
