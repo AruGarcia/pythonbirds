7be63601b0becd5a0cf0683a6616c0be5229f7f8
Aruana <aruanagarcia@gmail.com>
2023-06-26 11:53:29 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,21 @@
 class Pessoa:
-    def __init__(self,  nome=None, idade=43):
+    def __init__(self, *filhos, nome=None, idade=43):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Eleonora')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Aruanã'
-    print(p.nome)
-    print(p.idade)
+    Aruana = Pessoa(nome='Aruanã')
+    eleonora = Pessoa(Aruana, nome='Eleonora')
+    print(Pessoa.cumprimentar(eleonora))
+    print(id(eleonora))
+    print(eleonora.cumprimentar())
+    print(eleonora.nome)
+    print(eleonora.idade)
+    for filho in eleonora.filhos:
+        print(filho.nome)
+
